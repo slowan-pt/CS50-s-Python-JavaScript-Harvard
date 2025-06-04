0381bd904c9c6d8a10b37efe22eeb8b613d3bff8
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-06-04 17:27:21 -0300

    feat: dicionários em python

---
+++ python/dicionaries.py
@@ -0,0 +1,9 @@
+casa = {"Sloan":"Niterói", "Marcia":"Manaus","Glenda":"Curitiba", "Bete":"Nhamundá"}
+
+#Pesquisar um elemento
+print (casa)
+print (casa["Marcia"])
+
+#Adicionar um novo elemento
+casa ["Hermo"] = "São Paulo"
+print (casa)
