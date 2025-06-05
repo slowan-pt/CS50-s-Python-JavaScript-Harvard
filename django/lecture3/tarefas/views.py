cedfce682de4c7dcc933f283aa08ae1cbac2d5de
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-06-05 17:42:30 -0300

    feat: layout base e extends nas páginas que o recebem

--- django/lecture3/tarefas/views.py
+++ django/lecture3/tarefas/views.py
@@ -6,3 +6,6 @@ def index (request):
     return render(request, "tarefas/index.html", {
         "tarefas": tarefas
     })
+
+def adicionar (request):
+    return render (request, "tarefas/adicionar.html")
