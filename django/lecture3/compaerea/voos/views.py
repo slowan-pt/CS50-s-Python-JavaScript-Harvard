30c1b294ae9cda03a5c1193e53ebb9bba3f8864d
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-06-10 22:14:36 -0300

    feat: Django: INtrodução, SQL, shell, Models, Migrations, Admin

---
+++ django/lecture3/compaerea/voos/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from .models import Flight
+
+# Create your views here.
+def index (request):
+    return render (request, "voos/index.html", {
+        "voos": Flight.objects.all(),
+        })
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+        return render(request, "voos/flight.html", {
+            "voo": flight  # Passa apenas o objeto flight
+        })
+    except Flight.DoesNotExist:
+        return render(request, "voos/not_found.html")
