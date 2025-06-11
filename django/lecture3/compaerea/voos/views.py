b6a7e94c3170a98f490e859b2e70a0c6eebb7d0c
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-06-10 23:20:14 -0300

    feat: ajustes em layout de index.html, views.py e permitir que adicione passageiro direto na página do voo, por meio de um form com option

--- django/lecture3/compaerea/voos/views.py
+++ django/lecture3/compaerea/voos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index (request):
@@ -13,7 +15,20 @@ def flight(request, flight_id):
         flight = Flight.objects.get(pk=flight_id)
         return render(request, "voos/flight.html", {
             "voo": flight,  
-            "passageiros": flight.passengers.all()  
+            "passageiros": flight.passengers.all(),
+            "sem_passageiros": Passenger.objects.exclude(flights=flight).all()  # Mantido como sem_passageiros
         })
     except Flight.DoesNotExist:
         return render(request, "voos/not_found.html")
+    
+def book(request, flight_id):
+    if request.method == "POST":
+        try:
+            flight = Flight.objects.get(pk=flight_id)
+            passenger = Passenger.objects.get(pk=int(request.POST["passenger_id"]))  
+            passenger.flights.add(flight)
+            return HttpResponseRedirect(reverse("flight", args=(flight_id,)))  
+        except (Flight.DoesNotExist, Passenger.DoesNotExist, KeyError, ValueError) as e:
+            return render(request, "voos/error.html", {
+                "message": "Erro ao reservar voo. Verifique os dados e tente novamente."
+            })
